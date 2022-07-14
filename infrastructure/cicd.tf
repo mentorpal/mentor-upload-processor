5490ea76ed4e5e753aebbbd46c6715fcf9f8e97f
Milan Simonovic <mbsimonovic@gmail.com>
2022-07-14 07:37:14 +0200

    switch to gitlab cicd pipeline

--- infrastructure/cicd.tf
+++ infrastructure/cicd.tf
@@ -5,21 +5,33 @@ Permission to use, copy, modify, and distribute this software and its documentat
 The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 */
 module "pipeline" {
-  source                  = "git@github.com:mentorpal/terraform-modules//modules/trunk_cicd_pipeline?ref=tags/v1.2.1"
+  source                  = "git@github.com:mentorpal/terraform-modules//modules/gitflow_cicd_pipeline?ref=tags/v1.6.1"
   codestar_connection_arn = var.codestar_connection_arn
   project_name            = "mentor-upload-processor"
   github_repo_name        = "mentor-upload-processor"
-  build_cache_type        = "NO_CACHE"
-  deploy_cache_type       = "NO_CACHE"
-  build_compute_type      = "BUILD_GENERAL1_SMALL"
-  deploys_compute_type    = "BUILD_GENERAL1_SMALL"
+  github_org              = "mentorpal"
+  github_branch_dev       = "main"
+  github_branch_release   = "release"
 
-  build_buildspec             = "cicd/buildspec.yml"
-  deploy_staging_buildspec    = "cicd/deployspec_staging.yml"
-  deploy_prod_buildspec       = "cicd/deployspec_prod.yml"
-  deploys_privileged_mode     = true
-  export_pipeline_info        = true
+  enable_e2e_tests            = false
   enable_status_notifications = true
+  build_buildspec             = "cicd/buildspec.yml"
+  deploy_dev_buildspec        = "cicd/deployspec-dev.yml"
+  deploy_qa_buildspec         = "cicd/deployspec-qa.yml"
+  deploy_prod_buildspec       = "cicd/deployspec-prod.yml"
+
+  # reference: https://github.com/cloudposse/terraform-aws-codebuild#inputs
+  build_image  = "aws/codebuild/standard:5.0"
+  deploy_image = "aws/codebuild/standard:5.0"
+  # https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-compute-types.html
+  build_compute_type       = "BUILD_GENERAL1_SMALL"
+  deploys_compute_type     = "BUILD_GENERAL1_SMALL"
+  build_cache_type         = "LOCAL"
+  deploy_cache_type        = "LOCAL"
+  build_local_cache_modes  = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE"]
+  deploy_local_cache_modes = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE"]
+  builds_privileged_mode   = true
+  deploys_privileged_mode  = true
 
   tags = {
     Source = "terraform"
