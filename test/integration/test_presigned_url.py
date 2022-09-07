af8f2c299be987185cd2c5c5ce2d049db2750a4e
Milan Simonovic <mbsimonovic@gmail.com>
2022-09-07 09:43:51 +0200

    refactor

--- test/integration/test_presigned_url.py
+++ test/integration/test_presigned_url.py
@@ -1,10 +1,10 @@
 import requests
-from .util import headers, api_endpoint
+from .util import get_auth_headers, api_endpoint
 
 
 def test_upload_url():
-    response = requests.get(f"{api_endpoint}/upload/url", headers=headers)
-    print(headers, response.status_code, response.text)
+    response = requests.get(f"{api_endpoint}/upload/url", headers=get_auth_headers())
+    print(response.status_code, response.text) # for debugging if test fails
     assert response.status_code == 200
     assert "AWSAccessKeyId" in response.text
     assert "url" in response.text
--- test/integration/util.py
+++ test/integration/util.py
@@ -6,14 +6,17 @@ from module.utils import (
 
 jwt_secret = require_env("JWT_SECRET")
 api_endpoint = require_env("UPLOAD_ENDPOINT")
-token = {
-    "id": "6109d2a86e6fa01e5bf3219f",
-    "role": "ADMIN",
-    "mentorIds": ["not required"],
-}
-encoded = jwt.encode(token, jwt_secret, algorithm="HS256")
 
-headers = {
-    "Authorization": f"Bearer {encoded}",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-}
+def get_auth_headers(id="6109d2a86e6fa01e5bf3219f", mentorIds=["not required"]):
+    token = {
+        "id": id,
+        "role": "ADMIN",
+        "mentorIds": mentorIds,
+    }
+    encoded = jwt.encode(token, jwt_secret, algorithm="HS256")
+
+    headers = {
+        "Authorization": f"Bearer {encoded}",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+    }
+    return headers
